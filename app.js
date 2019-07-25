24f59284e5713232b227517915a628e3f664ce77
Jon Ransom <jransom12@live.com>
2019-07-25 13:08:15 -0500

    Game Plan commented

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ Build all of your functions for displaying and gathering information below (GUI)
 
 // app is the function called to start the entire application
 function app(people){
-  var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+  let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch(searchType){
     case 'yes':
-      var foundPerson = searchByName(people);
+      let foundPerson = searchByName(people);
       mainMenu(foundPerson, people);
       break;
     case 'no':
+    //use yesNo function to search for traits in yes no tree
       // TODO: search by traits
+      // alerts for each person returned for each criteria
+      // call this function Guess Who, start with criteria with largest search result, i.e. age, eye color, in descending amounts
       break;
       default:
-    app(people); // restart app
+    app(people); // restart app //create an alert saying no match found
       break;
   }
 }
@@ -24,22 +27,22 @@ function app(people){
 function mainMenu(person, people){
 
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
+//dynamically add spouse name
   if(!person){
     alert("Could not find that individual.");
     return app(people); // restart
   }
 
-  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
-
+  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+  // Create a function to gather info for each case with an alert
   switch(displayOption){
     case "info":
     // TODO: get person's info
     break;
-    case "family":
+    case "family": //descendants + spouse
     // TODO: get person's family
     break;
-    case "descendants":
+    case "descendants": //need recursion here
     // TODO: get person's descendants
     break;
     case "restart":
@@ -53,10 +56,10 @@ function mainMenu(person, people){
 }
 
 function searchByName(people){
-  var firstName = promptFor("What is the person's first name?", chars);
-  var lastName = promptFor("What is the person's last name?", chars);
+  let firstName = promptFor("What is the person's first name?", chars);
+  let lastName = promptFor("What is the person's last name?", chars);
 
-  var foundPerson = people.filter(function(person){
+  let foundPerson = people.filter(function(person){
     if(person.firstName === firstName && person.lastName === lastName){
       return true;
     }
@@ -64,9 +67,18 @@ function searchByName(people){
       return false;
     }
   })
+
+  // let morePeople = people;
+ 	// morePeople = searchbyGender
+ 	// 	morePeople = searchbyAge(morePeople);
+  // Dynamically add age
+  // Dynamically add alternate names and nicknames, such as Bob for Robert, or The Grey Man, or Flamingo
+  // Create function that finds a partial match, take name and divide into separate characters then search until no match
   // TODO: find the person using the name they entered
   return foundPerson;
 }
+
+//Create function searchbyCriteria
 
 // alerts a list of people
 function displayPeople(people){
@@ -74,24 +86,24 @@ function displayPeople(people){
     return person.firstName + " " + person.lastName;
   }).join("\n"));
 }
-
+// When looking for descendants or family(link it to the main menu)
 function displayPerson(person){
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
-  var personInfo = "First Name: " + person.firstName + "\n";
+  let personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
+  // TODO: finish getting the rest of the information to display(basically going to show the object)
   alert(personInfo);
 }
 
 // function that prompts and validates user input
 function promptFor(question, valid){
   do{
-    var response = prompt(question).trim();
+    let response = prompt(question).trim();
   } while(!response || !valid(response));
   return response;
 }
-
+// promptFor function ensures they answer the question with our provided options, in this case 'yes' or 'no'
 // helper function to pass into promptFor to validate yes/no answers
 function yesNo(input){
   return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
